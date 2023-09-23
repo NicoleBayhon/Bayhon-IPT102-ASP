8bffc6a98241a2ddea0a4588e2a372bc05ed4dcf
unknown <nbayhon15@gmail.com>
2023-09-23 12:42:03 +0800

    Updated student list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,10 +6,11 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" },
-            new StudentInfoModel { Name = "Nicole Bayhon", StudentId = "S21147", StudentAge = "20" }
+            new StudentInfoModel { Name = "Nicole Bayhon", StudentId = "21-1471", StudentAge = "20" },
+            new StudentInfoModel { Name = "Marilou Bayhon", StudentId = "21-2342", StudentAge = "55" },
+            new StudentInfoModel { Name = "Vicente Bayhon", StudentId = "21-4578", StudentAge = "60" }
+    
+            
         };
 
         return View(studentInfoArray);
